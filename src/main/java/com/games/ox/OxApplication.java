a89f9dce69f9503244d11384fc0fd65a76697385
Marcin <marcin.macina@outlook.com>
2022-08-25 18:05:13 +0200

    First commit

---
+++ src/main/java/com/games/ox/OxApplication.java
@@ -0,0 +1,13 @@
+package com.games.ox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OxApplication.class, args);
+    }
+
+}
